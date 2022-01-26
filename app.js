0f5530f03717bb48c1a359442c067fa4e3e43112
basispice <basil.breton@gmail.com>
2022-01-26 15:35:30 -0800

    mobile is done for now. Will add more if i get time. Functioning nav bar. Started media query that works on projects so far

--- app.js
+++ app.js
@@ -1,24 +1,82 @@
-console.log('im working')
+console.log("im working");
 
-//Tutorial for Scroll to click that I used (http://html-tuts.com/scroll-to-section-jquery/)
-$('.scrollTo').click(function() {
-	let getElem = $(this).attr('href');
-	let targetDistance = 20;
-	if ($(getElem).length) {
-		let getOffset = $(getElem).offset().top;
-		$('html,body').animate({
-			scrollTop: getOffset - targetDistance
-		}, 500);
-	}
-	return false;
-});
+$("#langNav").onclick = function () {
+  $("html, body").animate(
+    {
+      scrollTop: $(".langTitle").offset().top,
+    },
+    2000
+  );
+};
 
-function scrollTo(id) {
-	if ($(id).length) {
-		let getOffset = $(id).offset().top;
-		let targetDistance = 20;
-		$('html,body').animate({
-			scrollTop: getOffset - targetDistance
-		}, 500);
-	}
-}
+$("#projectsNav").onclick = function () {
+  $("html, body").animate(
+    {
+      scrollTop: $(".projects-title").offset().top,
+    },
+    2000
+  );
+};
+$("#languages").onclick = function () {
+  $("html, body").animate(
+    {
+      scrollTop: $(".#languages").offset().top,
+    },
+    2000
+  );
+};
+
+$("#contactMe").onclick = function () {
+	$("html, body").animate(
+	  {
+		scrollTop: $("#contactMe").offset().bottom,
+	  },
+	  2000
+	);
+  };
+
+
+//Tutorial for Scroll to click that I used but never worked (http://html-tuts.com/scroll-to-section-jquery/)
+// $('.scrollToLanguages').click(function() {
+// 	let getElem = $(this).attr('href');
+// 	let targetDistance = 20;
+// 	if ($(getElem).length) {
+// 		let getOffset = $(getElem).offset().top;
+// 		$('html,body').animate({
+// 			scrollTop: getOffset - targetDistance
+// 		}, 500);
+// 	}
+// 	return false;
+// });
+
+// function scrollToLanguages(id) {
+// 	if ($(id).length) {
+// 		let getOffset = $(id).offset().top;
+// 		let targetDistance = 20;
+// 		$('html,body').animate({
+// 			scrollTop: getOffset - targetDistance
+// 		}, 500);
+// 	}
+// }
+
+// $('.scrollToContact').click(function() {
+// 	let getElem = $(this).attr('href');
+// 	let targetDistance = 20;
+// 	if ($(getElem).length) {
+// 		let getOffset = $(getElem).offset().top;
+// 		$('html,body').animate({
+// 			scrollTop: getOffset - targetDistance
+// 		}, 500);
+// 	}
+// 	return false;
+// });
+
+// function scrollToContact(id) {
+// 	if ($(id).length) {
+// 		let getOffset = $(id).offset().top;
+// 		let targetDistance = 20;
+// 		$('html,body').animate({
+// 			scrollTop: getOffset - targetDistance
+// 		}, 500);
+// 	}
+// }
