e157e247d72c6c58a8a367458f8682d228adb537
basispice <basil.breton@gmail.com>
2022-01-25 16:40:36 -0800

    fixing the form the lazy way

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 console.log('im working')
 
 //Tutorial for Scroll to click that I used (http://html-tuts.com/scroll-to-section-jquery/)
-// $('.scrollTo').click(function() {
-// 	let getElem = $(this).attr('href');
-// 	let targetDistance = 20;
-// 	if ($(getElem).length) {
-// 		var getOffset = $(getElem).offset().top;
-// 		$('html,body').animate({
-// 			scrollTop: getOffset - targetDistance
-// 		}, 500);
-// 	}
-// 	return false;
-// });
+$('.scrollTo').click(function() {
+	let getElem = $(this).attr('href');
+	let targetDistance = 20;
+	if ($(getElem).length) {
+		let getOffset = $(getElem).offset().top;
+		$('html,body').animate({
+			scrollTop: getOffset - targetDistance
+		}, 500);
+	}
+	return false;
+});
 
-// function scrollTo(id) {
-// 	if ($(id).length) {
-// 		var getOffset = $(id).offset().top;
-// 		var targetDistance = 20;
-// 		$('html,body').animate({
-// 			scrollTop: getOffset - targetDistance
-// 		}, 500);
-// 	}
-// }
+function scrollTo(id) {
+	if ($(id).length) {
+		let getOffset = $(id).offset().top;
+		let targetDistance = 20;
+		$('html,body').animate({
+			scrollTop: getOffset - targetDistance
+		}, 500);
+	}
+}
