f8b52f18f1d6e1628848d81b9a1018b63a1e6320
basispice <basil.breton@gmail.com>
2022-01-25 16:26:49 -0800

    nav bar scrolls, broke everything else tho

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 console.log('im working')
+
+
+// $('.scrollTo').click(function() {
+// 	let getElem = $(this).attr('href');
+// 	let targetDistance = 20;
+// 	if ($(getElem).length) {
+// 		var getOffset = $(getElem).offset().top;
+// 		$('html,body').animate({
+// 			scrollTop: getOffset - targetDistance
+// 		}, 500);
+// 	}
+// 	return false;
+// });
+
+// function scrollTo(id) {
+// 	if ($(id).length) {
+// 		var getOffset = $(id).offset().top;
+// 		var targetDistance = 20;
+// 		$('html,body').animate({
+// 			scrollTop: getOffset - targetDistance
+// 		}, 500);
+// 	}
+// }
